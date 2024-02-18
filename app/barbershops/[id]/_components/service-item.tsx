7cb4c9d5bdbe8d52b9efbe831dd4033b6e63eaf3
Arthur Alexi <arthuralexi@hotmail.com>
2024-02-18 15:40:12 -0300

    feat: add initial ServiceItem  Component

---
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/app/_components/ui/button"
+import { Card, CardContent } from "@/app/_components/ui/card"
+import { Service } from "@prisma/client"
+import Image from "next/image"
+
+interface ServiceItemProps {
+    service: Service
+}
+
+const ServiceItem = ({ service }: ServiceItemProps) => {
+    return (
+        <Card>
+            <CardContent className="p-3 w-full">
+                <div className="flex gap-4 items-center w-full">
+                    <div className="relative min-w-[110px] min-h-[110px] max-w-[110px] max-h-[110px]">
+                        <Image src={service.imageUrl} alt={service.name} fill style={{ objectFit: 'contain' }}
+                            className="rounded-lg " />
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <h2 className="font-bold">{service.name}</h2>
+                        <p className="tex-sm text-gray-400">{service.description}</p>
+                        <div className="flex items-center justify-between mt-3">
+                            <p className="text-primary text-sm font-bold">
+                                {Intl.NumberFormat("pt-BR", {
+                                    style: "currency",
+                                    currency: "BRL",
+                                }).format(Number(service.price))}
+                            </p>
+                            <Button className="text-primary" variant='secondary'>reserve</Button>
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ServiceItem
